600b895dd9e6c619fb9c0fa7d850f1e663f6265a
Kcngu01 <120294672+Kcngu01@users.noreply.github.com>
2025-06-27 21:23:40 +0800

    add icon to header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
